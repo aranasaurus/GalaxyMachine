8bb2076047ce614a3d085337977318334b53bc50
Ryan Arana <ryan.arana@gmail.com>
2018-05-06 20:37:46 -0700

    Get ARKit working at a basic level

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import SceneKit
+import ARKit
+
 import GalaxyKit
 
 class ViewController: UIViewController {
-    fileprivate var sceneView: SCNView! { return view as? SCNView }
-    fileprivate var sectorScene: Sector.Scene? { return sceneView.scene as? Sector.Scene }
+    fileprivate var sceneView: ARSCNView! { return view as? ARSCNView }
+    fileprivate var config: ARWorldTrackingConfiguration!
 
     @IBOutlet fileprivate var starLabel: UILabel!
     @IBOutlet fileprivate var sectorLabel: UILabel!
@@ -23,22 +25,18 @@ class ViewController: UIViewController {
 
     fileprivate let galaxy: Galaxy
 
-    fileprivate var currentStarIndex: Int? {
+    fileprivate var currentStar: Star? {
         didSet {
             guard let currentStar = currentStar else { return }
-            starLabel.text = "\(currentStarIndex!): \(currentStar.description) (\(currentStar.coordinate))"
+            starLabel.text = "\(currentStar.description) (\(currentStar.coordinate))"
 
             temperatureLabel.text = "temp: \(formatter.string(from: currentStar.temperature))"
             luminosityLabel.text = "lumi: \(formatter.string(from: currentStar.luminosity))"
             massLabel.text = "mass: \(formatter.string(from: currentStar.mass))"
             radiusLabel.text = "rad:  \(formatter.string(from: currentStar.radius))"
-            
-            print("[\(currentStarIndex!)]\(currentStar.debugDescription)")
+
+            print("\(currentStar.debugDescription)")
         }
-    }
-    fileprivate var currentStar: Star? {
-        guard let index = currentStarIndex, index <= sectorScene?.sector.stars.endIndex ?? -1 else { return nil }
-        return sectorScene?.sector.stars[index]
     }
 
     fileprivate var formatter: MeasurementFormatter = {
@@ -68,47 +66,50 @@ class ViewController: UIViewController {
         let sector = galaxy[37, 47] // 37, 47 has a B0 in it!
         sceneView.backgroundColor = .black
         sceneView.autoenablesDefaultLighting = true
-        sceneView.allowsCameraControl = true
         sceneView.showsStatistics = true
 
+        config = ARWorldTrackingConfiguration()
+        config.planeDetection = .horizontal
+        sceneView.session.run(config)
+
         configure(for: sector)
+
+        sceneView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(rec:))))
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        sceneView.session.run(config)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+
+    @objc func handleTap(rec: UITapGestureRecognizer) {
+        guard rec.state == .ended else { return }
+        let location: CGPoint = rec.location(in: sceneView)
+        let hits = sceneView.hitTest(location, options: nil)
+
+        guard let node = hits.first(where: { $0.node is Star.Node })?.node as? Star.Node else { return }
+
+        currentStar = node.star
     }
     
     func configure(for sector: Sector) {
         sectorLabel.text = "Sector [\(sector.x), \(sector.y)] (\(sector.stars.count) systems)"
-        sceneView.scene = Sector.Scene(sector: sector, in: galaxy)
-        currentStarIndex = sectorScene?.sector.stars.startIndex
-    }
-    
-    @IBAction private func nextButtonTapped() {
-        guard let stars = sectorScene?.sector.stars else { return }
-        guard let currentIndex = currentStarIndex else {
-            currentStarIndex = stars.startIndex
-            return
+        for star in sector.stars {
+            sceneView.scene.rootNode.addChildNode(star.createNode(unit: .solarRadii))
         }
-
-        var nextIndex = stars.index(after: currentIndex)
-        if nextIndex == stars.endIndex {
-            nextIndex = stars.startIndex
-        }
-        currentStarIndex = nextIndex
-    }
-
-    @IBAction private func prevButtonTapped() {
-        guard let stars = sectorScene?.sector.stars else { return }
-        guard let currentIndex = currentStarIndex else {
-            currentStarIndex = stars.startIndex
-            return
-        }
-
-        guard currentIndex != stars.startIndex else {
-            currentStarIndex = stars.index(before: stars.endIndex)
-            return
-        }
-        currentStarIndex = stars.index(before: currentIndex)
+        currentStar = sector.stars.first
     }
 
     @IBAction private func changeSectorTapped() {
+        for node in sceneView.scene.rootNode.childNodes {
+            node.removeFromParentNode()
+        }
+
         let x = UInt(arc4random_uniform(64))
         let y = UInt(arc4random_uniform(64))
         configure(for: galaxy[x, y])
--- util/Star+SceneKit.swift
+++ util/Star+SceneKit.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import SceneKit
+
 import GalaxyKit
 
 extension Star {
@@ -20,22 +21,25 @@ extension Star {
         m.cullMode = .back
         return m
     }
-    
-    class Geometry: SCNSphere {
+
+    func createNode(unit: UnitLength) -> SCNNode {
+        return Node(star: self, unit: unit)
+    }
+
+    class Node: SCNNode {
+        let star: Star
         
-        init(star: Star) {
+        init(star: Star, unit: UnitLength = .solarRadii) {
+            self.star = star
             super.init()
-            
-            configureForStar(star)
+
+            self.geometry = SCNSphere(radius: CGFloat(star.radius.in(unit)))
+            self.geometry?.materials = [star.createMaterial()]
+            self.position = SCNVector3(Float(star.coordinate.x), Float(star.coordinate.y), Float(star.coordinate.z))
         }
 
         required init?(coder aDecoder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
-        }
-        
-        func configureForStar(_ star: Star) {
-            radius = CGFloat(star.radius.in(.solarRadii))
-            materials = [star.createMaterial()]
         }
     }
 }
